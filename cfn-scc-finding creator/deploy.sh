b91a996114a8e8139288caf20e222578567f2c8f
Garreth Benton <garreth.benton@gmail.com>
2023-05-17 17:50:00 +0200

    Update deploy.sh

--- cfn-scc-finding creator/deploy.sh
+++ cfn-scc-finding creator/deploy.sh
@@ -4,5 +4,5 @@ gcloud functions deploy cfn-scc-finding-creator \
                         --trigger-topic=org-audit-log-topic \
                         --ingress-settings=internal-only \
                         --region us-central1 \ # Update region
-                        --project= PROJECT \ # update project
+                        --project= [PROJECT] \ # update project
                         --entry-point=createSccFinding 
