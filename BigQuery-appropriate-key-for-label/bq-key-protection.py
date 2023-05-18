9e9e59801fc7a563a59ea5cae63a72f5bf30a612
Garreth Benton <garreth.benton@gmail.com>
2023-05-18 13:30:57 +0200

    Update bq-key-protection.py

--- BigQuery-appropriate-key-for-label/bq-key-protection.py
+++ BigQuery-appropriate-key-for-label/bq-key-protection.py
@@ -1,9 +1,14 @@
 from google.cloud import bigquery
 from google.cloud import securitycenter_v1beta1 as securitycenter
 
-def check_cai_bigquery_instance(project_id, dataset_id, table_id):
+def check_cai_bigquery_instance():
     # Instantiate the BigQuery client
     client = bigquery.Client()
+
+    # Retrieve the project ID, dataset ID, and table ID from the environment
+    project_id = client.project
+    dataset_id = "your-dataset-id"
+    table_id = "your-table-id"
 
     # Get the BigQuery table reference
     table_ref = client.dataset(dataset_id).table(table_id)
@@ -34,16 +39,24 @@ def create_scc_alert(project_id, source_id, finding_id):
     # Instantiate the SCC client
     client = securitycenter.SecurityCenterClient()
 
-    # Create the SCC finding name
-    finding_name = f"organizations/{project_id}/sources/{source_id}/findings/{finding_id}"
+    # Retrieve the SCC project ID from the environment
+    scc_project_id = client.project_path(project_id)
 
-    # Create the SCC alert request
+    # Retrieve the SCC source ID and finding ID from the environment or generate them as needed
+    scc_source_id = "your-scc-source-id"
+    scc_finding_id = "your-scc-finding-id"
+
+    # Create the SCC finding name
+    finding_name = f"organizations/{scc_project_id}/sources/{scc_source_id}/findings/{scc_finding_id}"
+
+    # Create the SCC alert request with a severity of MEDIUM
     request = securitycenter.CreateFindingRequest(
-        parent=f"organizations/{project_id}",
-        finding_id=finding_id,
+        parent=f"organizations/{scc_project_id}",
+        finding_id=scc_finding_id,
         finding={
             "name": finding_name,
             "state": securitycenter.Finding.State.ACTIVE,
+            "severity": securitycenter.Finding.Severity.MEDIUM,
             "source_properties": {
                 "status": "open"
             },
@@ -60,13 +73,8 @@ def create_scc_alert(project_id, source_id, finding_id):
 
     return response
 
-# Provide the project ID, dataset ID, and table ID
-project_id = "your-project-id"
-dataset_id = "your-dataset-id"
-table_id = "your-table-id"
-
-# Call the function to retrieve the project ID, dataset ID, table ID, label, encryption details, and protection level
-project_id, dataset_id, table_id, label, encryption_config, key_name, protection_level = check_cai_bigquery_instance(project_id, dataset_id, table_id)
+# Call the function to retrieve the necessary details
+project_id, dataset_id, table_id, label, encryption_config, key_name, protection_level = check_cai_bigquery_instance()
 
 # Print the results
 print("Project ID:", project_id)
@@ -79,10 +87,7 @@ print("Protection Level:", protection_level)
 
 # Check the label and protection level criteria
 if label == "Strictly Confidential" and protection_level not in ["HSM", "External", "External_VPC"]:
-    # Provide the SCC project ID, source ID, and finding ID
-    scc_project_id = "your-scc-project-id"
-    scc_source_id = "your-scc-source-id"
-    scc_finding_id = "your-scc-finding-id"
-
-    # Create an alert in SCC
-    response = create_scc_alert(scc_project_id,
+    # Create an alert with a severity
+    create_scc_alert(project_id, "your-scc-source-id", "your-scc-finding-id")
+    print("SCC alert created.") 
+   
