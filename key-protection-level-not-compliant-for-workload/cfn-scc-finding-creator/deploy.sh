aacb17dce1444ba5a7bc37a54b26f38700bbed36
Garreth Benton <garreth.benton@gmail.com>
2023-05-18 11:33:07 +0200

    Create deploy.sh

---
+++ key-protection-level-not-compliant-for-workload/cfn-scc-finding-creator/deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+gcloud functions deploy cfn-scc-finding-creator \
+                        --runtime=nodejs14 \
+                        --trigger-topic=org-audit-log-topic \
+                        --ingress-settings=internal-only \
+                        --region us-central1 \ # Update region
+                        --project= [PROJECT] \ # update project
+                        --entry-point=createSccFinding 
