031724b0746c13b5d25a09fcbe23639e8c84b49b
Garreth Benton <garreth.benton@gmail.com>
2023-05-18 13:07:59 +0200

    Update bq-key-protection.py
    Update to include publishing SCC finding

--- BigQuery-appropriate-key-for-label/bq-key-protection.py
+++ BigQuery-appropriate-key-for-label/bq-key-protection.py
@@ -1,4 +1,5 @@
 from google.cloud import bigquery
+from google.cloud import securitycenter_v1beta1 as securitycenter
 
 def check_cai_bigquery_instance(project_id, dataset_id, table_id):
     # Instantiate the BigQuery client
@@ -25,20 +26,63 @@ def check_cai_bigquery_instance(project_id, dataset_id, table_id):
         # Retrieve the protection level of the key
         protection_level = kms_key.get("protectionLevel")
 
-        return label, encryption_config, key_name, protection_level
+        return project_id, dataset_id, table_id, label, encryption_config, key_name, protection_level
     else:
-        return label, encryption_config, None, None
+        return project_id, dataset_id, table_id, label, encryption_config, None, None
+
+def create_scc_alert(project_id, source_id, finding_id):
+    # Instantiate the SCC client
+    client = securitycenter.SecurityCenterClient()
+
+    # Create the SCC finding name
+    finding_name = f"organizations/{project_id}/sources/{source_id}/findings/{finding_id}"
+
+    # Create the SCC alert request
+    request = securitycenter.CreateFindingRequest(
+        parent=f"organizations/{project_id}",
+        finding_id=finding_id,
+        finding={
+            "name": finding_name,
+            "state": securitycenter.Finding.State.ACTIVE,
+            "source_properties": {
+                "status": "open"
+            },
+            "security_marks": {
+                "marks": {
+                    "alert": "true"
+                }
+            }
+        }
+    )
+
+    # Send the alert request to SCC
+    response = client.create_finding(request)
+
+    return response
 
 # Provide the project ID, dataset ID, and table ID
 project_id = "your-project-id"
 dataset_id = "your-dataset-id"
 table_id = "your-table-id"
 
-# Call the function to retrieve the CAI, encryption details, and protection level
-label, encryption_config, key_name, protection_level = check_cai_bigquery_instance(project_id, dataset_id, table_id)
+# Call the function to retrieve the project ID, dataset ID, table ID, label, encryption details, and protection level
+project_id, dataset_id, table_id, label, encryption_config, key_name, protection_level = check_cai_bigquery_instance(project_id, dataset_id, table_id)
 
 # Print the results
+print("Project ID:", project_id)
+print("Dataset ID:", dataset_id)
+print("Table ID:", table_id)
 print("Label:", label)
 print("Encryption Configuration:", encryption_config)
 print("Key Name:", key_name)
 print("Protection Level:", protection_level)
+
+# Check the label and protection level criteria
+if label == "Strictly Confidential" and protection_level not in ["HSM", "External", "External_VPC"]:
+    # Provide the SCC project ID, source ID, and finding ID
+    scc_project_id = "your-scc-project-id"
+    scc_source_id = "your-scc-source-id"
+    scc_finding_id = "your-scc-finding-id"
+
+    # Create an alert in SCC
+    response = create_scc_alert(scc_project_id,
